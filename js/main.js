38b3635986a8cb438aa776b530d2399c7a519507
Brad Westfall <brad@azpixels.com>
2015-04-20 20:56:18 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function() {
                   div: mapElement,
                   lat: 33.4479042,
                   lng: -112.0620954,
-                  zoom: 11
+                  zoom: 9
                 });
 
                 // Plot
